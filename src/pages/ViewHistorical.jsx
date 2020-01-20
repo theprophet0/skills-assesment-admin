9523ffa3f8b9df9a1e8f297a2f135dfc477bdc6b
Nolan Jacobson <theprophet0@users.noreply.github.com>
2020-01-20 08:21:11 -0500

    added beared req on viewhistorical

--- src/pages/ViewHistorical.jsx
+++ src/pages/ViewHistorical.jsx
@@ -4,36 +4,56 @@ import { Button } from 'react-bootstrap'
 import ViewHistoricalComponent from '../components/ViewHistoricalComponent'
 import { Redirect } from 'react-router-dom'
 const ViewHistorical = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true)
+  const [token, setToken] = useState('')
+  useEffect(() => {
+    const successfulToken = localStorage.getItem('token')
+    if (!successfulToken) {
+      setIsAuthenticated(false)
+      getHistoricalRecords()
+    }
+    setToken(successfulToken)
+  }, [])
   const [historicalRecords, setHistoricalRecords] = useState([])
   const [redirect, setRedirect] = useState(false)
-  useEffect(() => {
-    const getHistoricalRecords = async () => {
-      const response = await axios.get(
-        'https://nurse-2-nurse-api.herokuapp.com/api​/NurseInformation​/All'
-      )
-      console.log(response)
-      setHistoricalRecords(response.data)
-    }
-    getHistoricalRecords()
-  }, [])
+  const getHistoricalRecords = async () => {
+    const response = await axios.get(
+      'https://nurse-2-nurse-api.herokuapp.com/api​/NurseInformation​/All',
+      { headers: { Authorization: 'Bearer ' + token } }
+    )
+    console.log(response)
+    setHistoricalRecords(response.data)
+  }
 
   return (
     <>
-      {redirect && <Redirect to="/" />}
-      <div className="centerButton">
-        <Button variant="primary" size="lg" onClick={() => setRedirect(true)}>
-          Add Recruiter
-        </Button>
-      </div>
-      <div className="reFlex">
-        {historicalRecords.map(record => {
-          return (
-            <div>
-              <ViewHistoricalComponent record={record} />
-            </div>
-          )
-        })}
-      </div>
+      {/* {redirect && <Redirect to="/home" />} */}
+      {isAuthenticated ? (
+        <>
+          <div className="centerButton">
+            <Button
+              variant="primary"
+              size="lg"
+              onClick={() => setRedirect(true)}
+            >
+              Add Recruiter
+            </Button>
+          </div>
+          <div className="reFlex">
+            {historicalRecords.map(record => {
+              return (
+                <div>
+                  <ViewHistoricalComponent record={record} />
+                </div>
+              )
+            })}
+          </div>
+        </>
+      ) : (
+        <>
+          <Redirect to="/" />
+        </>
+      )}
     </>
   )
 }
