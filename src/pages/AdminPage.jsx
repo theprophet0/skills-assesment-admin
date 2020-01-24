7ceb0dc4324c2cb486ac9d0ed64ff5605efdc26c
Nolan Jacobson <theprophet0@users.noreply.github.com>
2020-01-24 13:33:19 -0500

    added option to redirect to register

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Form, Button } from 'react-bootstrap'
-import { Redirect } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
 const AdminPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
