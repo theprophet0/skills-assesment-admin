7214ac5f3cb41a177a25afda9fe6497511b5f6a0
Nolan Jacobson <theprophet0@users.noreply.github.com>
2020-01-27 12:27:21 -0500

    fixed blank:block issue

--- src/pages/ViewHistorical.jsx
+++ src/pages/ViewHistorical.jsx
@@ -18,12 +18,47 @@ const ViewHistorical = () => {
       setLoaded(true)
     }
   }
+
+  function download_file(fileURL, fileName) {
+    // for non-IE
+    if (!window.ActiveXObject) {
+        var save = document.createElement('a');
+        save.href = fileURL;
+        save.target = '_blank';
+        var filename = fileURL.substring(fileURL.lastIndexOf('/')+1);
+        save.download = fileName || filename;
+	       if ( navigator.userAgent.toLowerCase().match(/(ipad|iphone|safari)/) && navigator.userAgent.search("Chrome") < 0) {
+				document.location = save.href; 
+// window event not working here
+			}else{
+		        var evt = new MouseEvent('click', {
+		            'view': window,
+		            'bubbles': true,
+		            'cancelable': false
+		        });
+		        save.dispatchEvent(evt);
+		        (window.URL || window.webkitURL).revokeObjectURL(save.href);
+			}	
+    }
+
+    // for IE < 11
+    else if ( !! window.ActiveXObject && document.execCommand)     {
+        var _window = window.open(fileURL, '_blank');
+        _window.document.close();
+        _window.document.execCommand('SaveAs', true, fileName || fileURL)
+        _window.close();
+    }
+}
   const getHistoricalRecordsWithPdf = async id => {
     const response = await axios.get(
       `https://new-nurse-2-nurse-api.herokuapp.com/api/NurseInformation/${id}`,
       { headers: { Authorization: 'Bearer ' + token } }
     )
-    window.open(response.data[0], '_blank')
+    
+   if (response.status === 200)
+   {
+     download_file(response.data[0], 'test')
+   }
   }
   const DeleteNurseRecord = async id => {
     const response = await axios.delete(
