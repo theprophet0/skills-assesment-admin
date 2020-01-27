5eaf1dd4d5db9f29896ca54a4a2d4a8614b6f360
Nolan Jacobson <theprophet0@users.noreply.github.com>
2020-01-27 12:35:47 -0500

    added spinner for load response

--- src/pages/ViewHistorical.jsx
+++ src/pages/ViewHistorical.jsx
@@ -19,46 +19,23 @@ const ViewHistorical = () => {
     }
   }
 
-  function download_file(fileURL, fileName) {
-    // for non-IE
-    if (!window.ActiveXObject) {
-        var save = document.createElement('a');
-        save.href = fileURL;
-        save.target = '_blank';
-        var filename = fileURL.substring(fileURL.lastIndexOf('/')+1);
-        save.download = fileName || filename;
-	       if ( navigator.userAgent.toLowerCase().match(/(ipad|iphone|safari)/) && navigator.userAgent.search("Chrome") < 0) {
-				document.location = save.href; 
-// window event not working here
-			}else{
-		        var evt = new MouseEvent('click', {
-		            'view': window,
-		            'bubbles': true,
-		            'cancelable': false
-		        });
-		        save.dispatchEvent(evt);
-		        (window.URL || window.webkitURL).revokeObjectURL(save.href);
-			}	
-    }
-
-    // for IE < 11
-    else if ( !! window.ActiveXObject && document.execCommand)     {
-        var _window = window.open(fileURL, '_blank');
-        _window.document.close();
-        _window.document.execCommand('SaveAs', true, fileName || fileURL)
-        _window.close();
-    }
-}
   const getHistoricalRecordsWithPdf = async id => {
     const response = await axios.get(
       `https://new-nurse-2-nurse-api.herokuapp.com/api/NurseInformation/${id}`,
       { headers: { Authorization: 'Bearer ' + token } }
     )
-    
-   if (response.status === 200)
-   {
-     download_file(response.data[0], 'test')
-   }
+
+    if (response.status === 200) {
+      const linkSource = `data:application/pdf;base64,${response.data[0].substring(
+        51
+      )}`
+      const downloadLink = document.createElement('a')
+      const fileName = 'vct_illustration.pdf'
+
+      downloadLink.href = linkSource
+      downloadLink.download = fileName
+      downloadLink.click()
+    }
   }
   const DeleteNurseRecord = async id => {
     const response = await axios.delete(
